f06d522a7f6ba6aaa654e0367d3bc87b7daa26e1
Rin <31349569+NagaseIori@users.noreply.github.com>
2022-07-10 22:08:38 +0800

    basics fix .gitignore

--- DyCore.cpp
+++ DyCore.cpp
@@ -4,7 +4,65 @@
 #include "framework.h"
 #include "DyCore.h"
 
+ChartInstance chart;
+
+// For DyNode
+vector<int> notePointers;
+
+DYCORE_API double DyCore_note_iterator_new() {
+	notePointers.push_back(0);
+	return notePointers.size() - 1;
+}
+
+DYCORE_API double DyCore_note_iterator_clear() {
+	notePointers.clear();
+	return true;
+}
+
+DYCORE_API double DyCore_note_iterator_read_real(double id, char* type) {
+	string nType(type);
+	NoteInstance inst = chart.get_note_at(id);
+
+	if (nType == "side") {
+		return (int)inst.side;
+	}
+	else if (nType == "width") {
+		return inst.width;
+	}
+	else if (nType == "position") {
+		return inst.position;
+	}
+	else if (nType == "time") {
+		return inst.time;
+	}
+	else if (nType == "type") {
+		return (int)inst.noteType;
+	}
+}
+
+DYCORE_API const char * DyCore_note_iterator_read_string(double id, char* type) {
+	string nType(type);
+	NoteInstance inst = chart.get_note_at(id);
+
+	if (nType == "nid") {
+		return inst.noteId.c_str();
+	}
+	else if (nType == "sid") {
+		return inst.subId.c_str();
+	}
+}
+
+DYCORE_API double DyCore_note_iterator_add(double id) {
+	notePointers[id] ++;
+	return true;
+}
+
 DYCORE_API const char* DyCore_init() {
 
 	return "success";
 }
+
+DYCORE_API double DyCore_chart_load(char* filePath) {
+
+}
+
--- DyCore.h
+++ DyCore.h
@@ -7,29 +7,52 @@ using std::string;
 using std::vector;
 using std::map;
 
+class NoteInstance;
+class ChartInstance;
+
 enum CHART_DIFFICULTY {
 	CASUAL, NORMAL, HARD, MEGA, GIGA, TERA
 };
+enum CHART_SIDE_TYPE {
+	MIXER, MULTI
+};
+enum CHART_SIDE {
+	DOWN, LEFT, RIGHT
+};
+enum NOTE_TYPE {
+	NOTE, CHAIN, HOLD, SUB
+};
 const string DIFF_STRING = "CNMHGT";
+const string CHART_SIDE_TYPE_STR[2] = { "MULTI", "MIXER" };
+
+CHART_SIDE_TYPE chart_sidetype_stoi(string stype) {
+	if (stype == "MULTI") return MULTI;
+	else if (stype == "MIXER") return MIXER;
+	else return CHART_SIDE_TYPE(-1);
+}
 
 
 
 class NoteInstance {
+public:
+	NOTE_TYPE noteType;
+	double width, position, time;
+	CHART_SIDE side;
 
+	string noteId = "", subId = "";
+
+	NoteInstance(NOTE_TYPE type, int wid, int pos, int tim, CHART_SIDE nside, string id, string sid) :
+		noteType(type), width(wid), position(pos), time(tim), side(nside), noteId(id), subId(sid)
+	{
+		if (type != HOLD) subId = "-1";
+	}
 };
 
 class ChartInstance {
 private:
-	enum CHART_SIDE_TYPE {
-		MIXER, MULTI
-	};
-	enum CHART_SIDE {
-		DOWN, LEFT, RIGHT
-	};
-
 	string chartTitle;
 
-	int chartSideType[2];
+	CHART_SIDE_TYPE chartSideType[2];
 
 	double chartBeatPerMinute, chartBarPerMinute, chartTimeOffset;
 
@@ -40,8 +63,9 @@ private:
 	vector<NoteInstance> chartNotesList;
 	map<string, NoteInstance> chartNotesMap[3];
 
-public:
+	
 
+public:
 	ChartInstance() {
 		chartID = chartMusicFilePath = chartFilePath = chartTitle = "";
 		chartDifficulty = CASUAL;
@@ -49,6 +73,33 @@ public:
 		chartBeatPerMinute = 180.0;
 		chartBarPerMinute = chartBeatPerMinute / 4;
 		chartTimeOffset = 0;
+
+
+	}
+
+	NoteInstance get_note_at(int id) {
+		return chartNotesList[id];
+	}
+
+	bool load_from_xml(string filePath) {
+		using namespace pugi;
+		
+		xml_document cxml;
+		cxml.load_file(filePath.c_str());
+
+		xml_node cmap = cxml.child("CMap");
+		chartTitle = cmap.child_value("m_path");
+		chartBarPerMinute = std::stof(cmap.child_value("m_barPerMin"));
+		chartTimeOffset = std::stof(cmap.child_value("m_timeOffset"));
+		chartSideType[0] = chart_sidetype_stoi(cmap.child_value("m_leftRegion"));
+		chartSideType[1] = chart_sidetype_stoi(cmap.child_value("m_rightRegion"));
+		chartID = cmap.child_value("m_mapID");
+
+		xml_node downside = cmap.child("m_notes").child("m_notes");
+
+		for (xml_node note : downside.children("CMapNoteAsset")) {
+
+		}
 	}
 };
 
