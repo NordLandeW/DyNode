29873bcdf336b89b0a38de4610de3c4f47ae2b78
Rin <31349569+NagaseIori@users.noreply.github.com>
2023-02-21 20:19:37 +0800

    refactor: Change project type to cmake

--- DyCore.cpp
+++ DyCore.cpp
@@ -1,7 +1,6 @@
-﻿// DyCore.cpp: Core of DyNode.
+﻿// DyCore.cpp: 定义应用程序的入口点。
+//
 
-#include "pch.h"
-#include "framework.h"
 #include "DyCore.h"
 
 const int RETURN_BUFFER_SIZE = 10 * 1024 * 1024;
@@ -21,20 +20,20 @@ DYCORE_API const char* DyCore_delaunator(char* in_struct) {
 	auto coords = j.get<std::vector<double>>();
 
 	delaunator::Delaunator d(coords);
-	
+
 	json r = json::array();
 
-    for (std::size_t i = 0; i < d.triangles.size(); i += 3) {
-        r.push_back(
-            {
-                {"p1", {d.coords[2 * d.triangles[i]], d.coords[2 * d.triangles[i] + 1]}},
-                {"p2", {d.coords[2 * d.triangles[i + 1]], d.coords[2 * d.triangles[i + 1] + 1]}},
+	for (std::size_t i = 0; i < d.triangles.size(); i += 3) {
+		r.push_back(
+			{
+				{"p1", {d.coords[2 * d.triangles[i]], d.coords[2 * d.triangles[i] + 1]}},
+				{"p2", {d.coords[2 * d.triangles[i + 1]], d.coords[2 * d.triangles[i + 1] + 1]}},
 				{"p3", {d.coords[2 * d.triangles[i + 2]], d.coords[2 * d.triangles[i + 2] + 1]}}
-            }
-        );
-    }
+			}
+		);
+	}
 
-	strcpy_s(_return_buffer, r.dump().c_str());
+	strcpy(_return_buffer, r.dump().c_str());
 
 
 	return _return_buffer;
--- DyCore.h
+++ DyCore.h
@@ -1,6 +1,19 @@
-﻿
-#define DYCORE_API extern "C" __declspec(dllexport)
-#include "pch.h"
+﻿#pragma once
+#include <vector>
+#include <string>
+#include <map>
+#include "json.hpp"
+#include "pugixml.hpp"
+#include "delaunator.hpp"
+#include <iostream>
+
+#if !defined( _MSC_VER)
+#define EXPORTED_FN __attribute__((visibility("default")))
+#else
+#define EXPORTED_FN __declspec(dllexport)
+#endif
+
+#define DYCORE_API extern "C" EXPORTED_FN
 
 using json = nlohmann::json;
 using std::string;
