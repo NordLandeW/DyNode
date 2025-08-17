63feb962280c5b18c0d43225da16aa98f3fe237a
NagaseIori <ajcxsu@gmail.com>
2025-08-17 21:48:07 +0800

    refactor(utils): Separate API functions into new source file
    Moves all `DYCORE_API` functions from `utils.cpp` to a new `utilsAPI.cpp` to improve code organization by separating the public API from internal implementations.
    
    - Adds a new `DyCore_irandom_range` function for generating random integers.
    - Fixes a bug where the random number generator was unseeded, causing it to produce the same sequence of numbers on each run.

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -1,18 +1,15 @@
-#include "gm.h"
+
 #define NOMINMAX
+#include "utils.h"
+
 #include <Windows.h>
 
-#include <algorithm>
 #include <chrono>
 #include <cstdio>
 #include <filesystem>
 #include <iostream>
 #include <random>
 #include <taskflow/taskflow.hpp>
-
-#include "api.h"
-#include "utils.h"
-
 
 namespace fs = std::filesystem;
 
@@ -126,43 +123,6 @@ char difficulty_int_to_char(int index) {
     return DIFFICULTY_STRING[index];
 }
 
-// Copies a block of memory from a source address to a destination address.
-DYCORE_API double DyCore_buffer_copy(void* dst, void* src, double size) {
-    memcpy(dst, src, (size_t)size);
-    return 0;
-}
-
-// Sorts an array of std::pair<double, double> in ascending order based on the
-// first element of the pair.
-DYCORE_API double DyCore_index_sort(void* data, double size) {
-    auto* pair_data = static_cast<std::pair<double, double>*>(data);
-    std::sort(pair_data, pair_data + (size_t)size);
-    return 0;
-}
-
-// Sorts an array of doubles.
-// The sorting order is determined by the 'type' parameter.
-//
-// @param data A pointer to the array of doubles.
-// @param size The number of elements in the array.
-// @param type If true, sorts in ascending order; otherwise, sorts in descending
-// order.
-DYCORE_API double DyCore_quick_sort(void* data, double size, double type) {
-    double* arr = static_cast<double*>(data);
-    if (type) {
-        std::sort(arr, arr + (size_t)size);
-    } else {
-        std::sort(arr, arr + (size_t)size, std::greater<double>());
-    }
-    return 0;
-}
-
-DYCORE_API const char* DyCore_random_string(double length) {
-    static std::string str;
-    str = random_string(int(length));
-    return str.c_str();
-}
-
 int hardware_concurrency() {
     return std::thread::hardware_concurrency();
 }
@@ -186,14 +146,4 @@ uint64_t get_current_time() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(
                std::chrono::system_clock::now().time_since_epoch())
         .count();
-}
-
-DYCORE_API double DyCore_random_range(double min, double max) {
-    if (min > max) {
-        throw_error_event("Invalid range");
-        return min;
-    }
-    static std::mt19937_64 mt19937;
-    std::uniform_real_distribution<double> dist(min, max);
-    return dist(mt19937);
 }
