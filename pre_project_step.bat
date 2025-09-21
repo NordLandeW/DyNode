f3396ff97db1767414f1fc02830328e34599d1ff
NagaseIori <ajcxsu@gmail.com>
2025-09-21 16:04:07 +0800

    feat: Optimize pre project step script

--- pre_project_step.bat
+++ pre_project_step.bat
@@ -1,13 +1,19 @@
 @echo off
 :: Build DyCore before running the project.
 
-echo --- Building DyCore...
+:: Set build mode here: "release" or "debug"
+set BUILD_MODE=release
+
+set CMAKE_PRESET=x64-%BUILD_MODE%
+set BUILD_DIR=out/build/x64-%BUILD_MODE%
+
+echo --- Building DyCore (%BUILD_MODE%)...
 
 :: Navigate to the DyCore directory.
 cd %YYprojectDir%/DyCore
 
 :: Configure and build the project.
-cmake --preset=x64-release
-cmake --build out/build/x64-release
+cmake --preset=%CMAKE_PRESET%
+cmake --build %BUILD_DIR%
 
 echo --- DyCore build finished.
