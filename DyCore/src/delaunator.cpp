f2f2c085b6c9bc4cb45c0ee393ddc877a7d94c49
NagaseIori <ajcxsu@gmail.com>
2025-10-27 13:07:40 +0800

    chore(delaunator): remove deprecated JSON-based function
    The `DyCore_delaunator` function, which operated on JSON strings, was previously marked as deprecated. It has been removed in favor of the more performant, buffer-based `DyCore_delaunator_buffer` interface.
    
    BREAKING CHANGE: The `DyCore_delaunator` function has been removed. Users must migrate to `DyCore_delaunator_buffer`.

--- DyCore/src/delaunator.cpp
+++ DyCore/src/delaunator.cpp
@@ -9,44 +9,6 @@
 
 using nlohmann::json;
 
-// ! Deprecated.
-// Performs Delaunay triangulation on a set of 2D points.
-//
-// @param in_struct A JSON string representing an array of coordinates.
-//        Example: "[x1, y1, x2, y2, ...]"
-// @return A JSON string representing an array of triangles.
-//         Returns an error message if the input is not a valid array.
-DYCORE_API const char* DyCore_delaunator(char* in_struct) {
-    static std::string returnBuffer;
-
-    json j = json::parse(in_struct);
-
-    if (!j.is_array())
-        return "Error!in_struct must be an array.";
-
-    auto coords = j.get<std::vector<double>>();
-
-    delaunator::Delaunator d(coords);
-
-    json r = json::array();
-
-    for (std::size_t i = 0; i < d.triangles.size(); i += 3) {
-        r.push_back(
-            {{"p1",
-              {d.coords[2 * d.triangles[i]], d.coords[2 * d.triangles[i] + 1]}},
-             {"p2",
-              {d.coords[2 * d.triangles[i + 1]],
-               d.coords[2 * d.triangles[i + 1] + 1]}},
-             {"p3",
-              {d.coords[2 * d.triangles[i + 2]],
-               d.coords[2 * d.triangles[i + 2] + 1]}}});
-    }
-
-    returnBuffer = r.dump();
-
-    return returnBuffer.c_str();
-}
-
 // Binary buffer interface (preferred). Input: [u32 point_count][x f64][y f64]*N
 // Output: [u32 tri_count][x1 f64][y1 f64][x2 f64][y2 f64][x3 f64][y3 f64]*tri
 // coordFormat: 0 = f32, 1 = f64 (both for input). Output uses f64 by design.
